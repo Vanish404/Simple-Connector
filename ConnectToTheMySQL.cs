ead4f8a63f5ffd3ec9ac9e1cd94c7951c8414d2e
Vanish404 <pro100stalkerss@gmail.com>
2015-07-11 00:11:16 +0300

    Реализована функция входа на MySQL Чтение некоторых данных Запись в главную форму

---
+++ ConnectToTheMySQL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MySQL
+{
+    class ConnectToMySQL
+    {
+        public DataTable GetComments(string server, string database, string username, string password)
+        {
+            DataTable dt = new DataTable();
+
+            MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder(); 
+            mysqlCSB.Server = server;
+            mysqlCSB.Database = database;
+            mysqlCSB.UserID = username;
+            mysqlCSB.Password = password;
+
+            string queryString = @"SELECT comment_author, 
+                                 comment_date,     
+                                 comment_content               
+                          FROM   fgel_comments
+                          WHERE  comment_date";
+            
+            MySqlConnection con = new MySqlConnection(mysqlCSB.ConnectionString); 
+            con.ConnectionString = mysqlCSB.ConnectionString;
+            MySqlCommand com = new MySqlCommand(queryString, con);
+
+            try
+            {
+                con.Open();
+
+                using (MySqlDataReader dr = com.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        dt.Load(dr);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+                con.Close();
+            
+            return dt;
+        }
+    }
+}
